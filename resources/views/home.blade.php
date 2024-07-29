52a21b5480ec4a26c8e918a478e8ba3df818e54b
Sayorita <saracf.436@gmail.com>
2024-07-29 11:34:13 -0300

    Estilizando a home

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -8,30 +8,73 @@
     <div class="container py-12">
         <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
             <div class="bg-white dark:bg-gray-800 overflow-hidden shadow-sm sm:rounded-lg">
-                <div class="p-6 text-gray-900 dark:text-gray-100">
-                   <h1> Notícias </h1>
-                   @if ($noticias->isEmpty())
-                   <p>Não há notícias disponíveis no momento.</p>
-                   @endif
+                <h1> Notícias </h1>
+                @if ($noticias->isEmpty())
+                    <p>Não há notícias disponíveis no momento.</p>
+                @endif
 
-                   <div class="row">
-                        @foreach ($noticias as $noticia)
-                            <div class="col-md4 mb-4">
-                                <div class="card">
-                                    @if ($noticia->url)
-                                        <img src="{{ asset($noticia->url)}}" alt="{{ $noticia->titulo}}">
-                                    @endif
+                <div id="carouselExampleCaptions" class="carousel slide">
+                    <div class="carousel-indicators">
+                        <button type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide-to="0"
+                            class="active" aria-current="true" aria-label="Slide 1"></button>
+                        <button type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide-to="1"
+                            aria-label="Slide 2"></button>
+                        <button type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide-to="2"
+                            aria-label="Slide 3"></button>
+                    </div>
+                    <div class="carousel-inner">
+                        <div class="carousel-item active">
+                            <img src="..." class="d-block w-100" alt="...">
+                            <div class="carousel-caption d-none d-md-block">
+                                <h5>First slide label</h5>
+                                <p>Some representative placeholder content for the first slide.</p>
+                            </div>
+                        </div>
+                        <div class="carousel-item">
+                            <img src="..." class="d-block w-100" alt="...">
+                            <div class="carousel-caption d-none d-md-block">
+                                <h5>Second slide label</h5>
+                                <p>Some representative placeholder content for the second slide.</p>
+                            </div>
+                        </div>
+                        <div class="carousel-item">
+                            <img src="..." class="d-block w-100" alt="...">
+                            <div class="carousel-caption d-none d-md-block">
+                                <h5>Third slide label</h5>
+                                <p>Some representative placeholder content for the third slide.</p>
+                            </div>
+                        </div>
+                    </div>
+                    <button class="carousel-control-prev" type="button" data-bs-target="#carouselExampleCaptions"
+                        data-bs-slide="prev">
+                        <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+                        <span class="visually-hidden">Previous</span>
+                    </button>
+                    <button class="carousel-control-next" type="button" data-bs-target="#carouselExampleCaptions"
+                        data-bs-slide="next">
+                        <span class="carousel-control-next-icon" aria-hidden="true"></span>
+                        <span class="visually-hidden">Next</span>
+                    </button>
+                </div>
+
+                <div class="row row-cols-2 row-cols-lg-4 g-2 g-lg-">
+                    @foreach ($noticias as $noticia)
+                        <div class="col-md4 mb-4">
+                            <div class="card" style="width: 18rem;">
+                                @if ($noticia->url)
+                                    <img src="{{ asset($noticia->url)}}" class="card-img-top" alt="{{ $noticia->titulo}}">
+                                @endif
                                 <div class="card-body">
                                     <h5 class="card-title">{{ $noticia->titulo}}</h5>
                                     <p class="card-text">{{ $noticia->descricao }}</p>
-                                </div>        
                                 </div>
                             </div>
-                        @endforeach
-                   </div>
+                        </div>
+                    @endforeach
                 </div>
             </div>
         </div>
+    </div>
     </div>
 
 </x-app-layout>
